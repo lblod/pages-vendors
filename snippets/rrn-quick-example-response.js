d55a34b034471db1c982ccc49ff338c6a3f250c6
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-05-04 12:08:44 +0200

    update context

--- snippets/rrn-quick-example-response.js
+++ snippets/rrn-quick-example-response.js
@@ -1,5 +1,5 @@
 {
-  "@context": "http://lblod.data.gift/contexts/rijksregisternummer.json",
+  "@context": "http://lblod.data.gift/contexts/rijksregisternummer-api/context.json",
   "uri": "http://data.lblod.info/personen/6e0c023c-137f-407c-ada0-31693c9038f5",
   "rrn": "84.04.17-319.23",
   "@type": "foaf:Person"
