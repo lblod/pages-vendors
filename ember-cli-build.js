f5fccd76ab5eaef7c8232321fb7fed409808e94e
benjay10 <ben.behaeghe@redpencil.io>
2022-11-23 22:15:41 +0100

    Upgrade some more non-Ember packages and no embroider
    Some more package bumps.
    I decide to stick to more of a default Ember experience, which does not
    include embroider.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,12 +20,5 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  //return app.toTree();
-  const Webpack = require('@embroider/webpack').Webpack;
-  return require('@embroider/compat').compatBuild(app, Webpack, {
-    staticAddonTrees: true,
-    staticAddonTestSupportTrees: true,
-    staticHelpers: true,
-    staticComponents: true
-  });
+  return app.toTree();
 };
