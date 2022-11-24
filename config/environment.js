1388f75563cc14993d13a4bc421e4ae80200e3eb
benjay10 <ben.behaeghe@redpencil.io>
2022-11-24 03:30:27 +0100

    Manual Linting fixes

--- config/environment.js
+++ config/environment.js
@@ -44,8 +44,7 @@ module.exports = function (environment) {
     ENV.APP.autoboot = false;
   }
 
-  if (environment === 'production') {
-  }
+  //if (environment === 'production') {}
 
   return ENV;
 };
