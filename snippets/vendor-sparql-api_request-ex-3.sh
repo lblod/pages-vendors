9db8cd63fa427316a8d9ece95edd4f501752bc17
benjay10 <ben.behaeghe@redpencil.io>
2024-04-17 18:36:21 +0200

    Restructure all code snippets
    Renamed to Dutch and given a more consistent and meaningful name.
    Also grouped into folders that correlate to the docs pages.

--- snippets/vendor-sparql-api_request-ex-3.sh
+++
@@ -1,6 +0,0 @@
-curl -v -X POST \
-  -H "Content-Type: application/sparql-query" \
-  -H "Accept: application/sparql-results+json" \
-  -b CookieJar.tsv -c CookieJar.tsv \
-  -d 'SELECT DISTINCT ?s ?p ?o WHERE { ?s ?p ?o . } LIMIT 10' \
-  https://api.loket.lokaalbestuur.vlaanderen.be/vendor/sparql
