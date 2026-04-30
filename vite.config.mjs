a81c60275760786b19ac5938b11b69f11cae4d87
benjay10 <ben.behaeghe@redpencil.io>
2026-04-30 16:38:40 +0200

    Conditional base URL in Vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,22 +2,24 @@ import { defineConfig } from 'vite';
 import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
-export default defineConfig({
-  base: '/pages-vendors/',
-  plugins: [
-    classicEmberSupport(),
-    ember(),
-    // extra plugins here
-    babel({
-      babelHelpers: 'runtime',
-      extensions,
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        loadPaths: ['./node_modules/'],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'development' ? '/' : '/pages-vendors/',
+    plugins: [
+      classicEmberSupport(),
+      ember(),
+      // extra plugins here
+      babel({
+        babelHelpers: 'runtime',
+        extensions,
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          loadPaths: ['./node_modules/'],
+        },
       },
     },
-  },
+  }
 });
