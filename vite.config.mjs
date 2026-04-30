fd6115d670c76996dbb8a49bc2bfe0fc597d18f3
benjay10 <ben.behaeghe@redpencil.io>
2026-04-30 03:42:57 +0200

    Base URL in Vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
 export default defineConfig({
+  base: '/pages-vendors/',
   plugins: [
     classicEmberSupport(),
     ember(),
